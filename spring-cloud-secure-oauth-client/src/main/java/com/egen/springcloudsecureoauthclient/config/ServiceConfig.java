3a01bc66593aad1852514ab1990537d99659a0d1
Chi Dov <cdov@netspend.com>
2018-05-06 03:58:20 -0500

    remove unused import switch service auth to github

--- spring-cloud-secure-oauth-client/src/main/java/com/egen/springcloudsecureoauthclient/config/ServiceConfig.java
+++ spring-cloud-secure-oauth-client/src/main/java/com/egen/springcloudsecureoauthclient/config/ServiceConfig.java
@@ -1,12 +1,10 @@
 package com.egen.springcloudsecureoauthclient.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 
 /**
  * @author cdov
