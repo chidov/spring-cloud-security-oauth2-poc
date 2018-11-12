23fbb1b515c25aafec955bd6ae0cd376ba983439
Chi Dov <cdov@netspend.com>
2018-11-11 20:47:50 -0600

    remove custom user info since scope customization will be handle DefaultTokenServices

--- spring-cloud-secure-service/src/main/java/com/egen/springcloudsecureservice/config/SecurityConfig.java
+++ spring-cloud-secure-service/src/main/java/com/egen/springcloudsecureservice/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.egen.springcloudsecureservice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 /**
  * @author cdov
@@ -16,19 +12,6 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends GlobalMethodSecurityConfiguration {
-
-    private final ResourceServerProperties sso;
-
-    @Autowired
-    public SecurityConfig(ResourceServerProperties sso) {
-        this.sso = sso;
-    }
-
-    //added
-    @Bean
-    public ResourceServerTokenServices myUserInfoTokenServices() {
-        return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
-    }
 
     @Override
     protected MethodSecurityExpressionHandler createExpressionHandler() {
